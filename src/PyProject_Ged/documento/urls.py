9b87a3a21be3eb91efe268c868b54faad9ad4ce0
Diego de Souza Costa <diego.costa@shiftit.com.br>
2012-07-11 16:08:49 -0300

    Adicionado telas de acoes, arvore de documentos e fancy box.

--- src/PyProject_Ged/documento/urls.py
+++ src/PyProject_Ged/documento/urls.py
@@ -3,6 +3,18 @@ from django.utils.translation   import ugettext_lazy as trans
 
 urlpatterns= patterns('documento.views',
                       url('^documentos/$', 'documentos',
-                          {'vTitulo': trans(u'socumentos')}, name='documentos'), 
+                          {'vTitulo': trans(u'documentos')}, name='documentos'), 
+                      url('^checkin/$', 'checkin',
+                          {'vTitulo': trans(u'checkin')}, name='checkin'),
+                      url('^importar_documento/$', 'importar',
+                          {'vTitulo': trans(u'importar')}, name='importar'),
+                      url('^aprovar_documento/$', 'aprovar',
+                          {'vTitulo': trans(u'aprovar')}, name='aprovar'),
+                      url('^reprovar_documento/$', 'reprovar',
+                          {'vTitulo': trans(u'reprovar')}, name='reprovar'),
+                      url('^excluir_documento/$', 'excluir',
+                          {'vTitulo': trans(u'excluir')}, name='excluir'),
+                      url('^informacoes_documento/$', 'informacoes',
+                          {'vTitulo': trans(u'informacoes')}, name='informacoes'),
 
                       )
--- src/PyProject_Ged/documento/views.py
+++ src/PyProject_Ged/documento/views.py
@@ -9,3 +9,51 @@ def documentos(vRequest, vTitulo):
         locals(),
         context_instance=RequestContext(vRequest),
         )
+    
+def checkin(vRequest, vTitulo):
+        
+    return render_to_response(
+        'documentos/checkin.html',
+        locals(),
+        context_instance=RequestContext(vRequest),
+        )
+    
+def importar(vRequest, vTitulo):
+        
+    return render_to_response(
+        'documentos/importar_doc.html',
+        locals(),
+        context_instance=RequestContext(vRequest),
+        )
+    
+def aprovar(vRequest, vTitulo):
+        
+    return render_to_response(
+        'acao/aprovar.html',
+        locals(),
+        context_instance=RequestContext(vRequest),
+        )
+    
+def reprovar(vRequest, vTitulo):
+        
+    return render_to_response(
+        'acao/reprovar.html',
+        locals(),
+        context_instance=RequestContext(vRequest),
+        )
+    
+def excluir(vRequest, vTitulo):
+        
+    return render_to_response(
+        'acao/excluir.html',
+        locals(),
+        context_instance=RequestContext(vRequest),
+        )
+    
+def informacoes(vRequest, vTitulo):
+        
+    return render_to_response(
+        'documentos/informacoes.html',
+        locals(),
+        context_instance=RequestContext(vRequest),
+        )
