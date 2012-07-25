d5a2b06a0c04054d55c341762d8d67fde5307df2
Alexandre Spengler <alexandre.spengler@gmail.com>
2012-07-25 17:12:57 -0300

    Adicionado suporte a multiempresas no Admin

--- src/PyProject_GED/multiAdmin.py
+++ src/PyProject_GED/multiAdmin.py
@@ -4,12 +4,10 @@ Created on Jul 10, 2012
 @author: Shift IT | wwww.shiftit.com.br
 '''
 from django.contrib                 import admin
-
-import constantes #@UnresolvedImport
+from autenticacao.models            import Usuario
 
 class MultiDBModelAdmin(admin.ModelAdmin):
     # A handy constant for the name of the alternate database.
-    using = 'default'
 
     def save_model(self, request, obj, form, change):
         # Tell Django to save objects to the 'other' database.
@@ -17,18 +15,26 @@ class MultiDBModelAdmin(admin.ModelAdmin):
 
     def delete_model(self, requqest, obj):
         # Tell Django to delete objects from the 'other' database
-        obj.delete(using=self.using)
+        obj.delete()
 
-    def queryset(self, request):
+    def queryset(self, vRequest):
         # Tell Django to look for objects on the 'other' database.
-        return super(MultiDBModelAdmin, self).queryset(request).using(self.using)
+        qs = super(MultiDBModelAdmin, self).queryset(vRequest)
+        iEmpresa= Usuario().obtemEmpresaDoUsuario(vRequest.user.id)
+        if iEmpresa != None:
+            try:
+                return qs.filter(empresa= iEmpresa)
+            except:
+                return qs.all()
+        else:
+            return qs.all()
 
     def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
         # Tell Django to populate ForeignKey widgets using a query
         # on the 'other' database.
-        return super(MultiDBModelAdmin, self).formfield_for_foreignkey(db_field, request=request, using=self.using, **kwargs)
+        return super(MultiDBModelAdmin, self).formfield_for_foreignkey(db_field, request=request, **kwargs)
 
     def formfield_for_manytomany(self, db_field, request=None, **kwargs):
         # Tell Django to populate ManyToMany widgets using a query
         # on the 'other' database.
-        return super(MultiDBModelAdmin, self).formfield_for_manytomany(db_field, request=request, using=self.using, **kwargs)
+        return super(MultiDBModelAdmin, self).formfield_for_manytomany(db_field, request=request, **kwargs)
