242160a2e9a6ebf775023ec2420de29ab9371bdc
Alexandre Spengler <alexandre.spengler@gmail.com>
2012-07-26 14:53:31 -0300

    Admin funcionando com multiempresa

--- src/PyProject_GED/autenticacao/admin.py
+++ src/PyProject_GED/autenticacao/admin.py
@@ -6,39 +6,59 @@ Created on Jan 17, 2012
 
 from django.contrib                 import admin
 from django.contrib.auth.admin      import UserAdmin 
+from django.contrib.auth.models     import User, Group
 
 from models                         import Empresa
 from models                         import Tipo_de_Usuario
 from models                         import Usuario
 from multiAdmin                     import MultiDBModelAdmin #@UnresolvedImport
-from django.contrib.admin.options   import ModelAdmin
 
 
 class AdminEmpresa(MultiDBModelAdmin): 
     list_display    = ('id_empresa', 'nome', 'cnpj', 'cep', 'rua', 'numero', 'complemento', 'bairro', 
                        'cidade', 'uf', 'eh_ativo')
     search_fields   = ('id_empresa', 'nome', 'cnpj', 'eh_ativo')
-    list_filter     = ('id_empresa', 'nome', 'cnpj', 'eh_ativo')
+    
+    def queryset(self, vRequest):
+        qs = super(MultiDBModelAdmin, self).queryset(vRequest)
+        iEmpresa= Usuario().obtemEmpresaDoUsuario(vRequest.user.id)
+        if iEmpresa != None:
+            try:
+                return qs.filter(id_empresa= iEmpresa)
+                self.fields['empresa']= iEmpresa
+            except:
+                return qs.all()
+        else:
+            return qs.all()
     
 class AdminTipo_de_Usuario(MultiDBModelAdmin): 
     list_display    = ('id_tipo_usuario', 'descricao')
     search_fields   = ('id_tipo_usuario',)
-    list_filter     = ('id_tipo_usuario',)
+    exclude         = ('empresa',) 
+
+    def save_model(self, request, obj, form, change):
+        iEmpresa= Usuario().obtemEmpresaDoUsuario(request.user.id)
+        obj.empresa = iEmpresa
+        obj.save()
 
 class AdminUsuario(MultiDBModelAdmin): 
     list_display    = UserAdmin.list_display + ('empresa', 'tipo_usuario', 'eh_ativo')
     search_fields   = UserAdmin.search_fields
-    list_filter     = UserAdmin.list_filter
+    exclude         = ('empresa',) 
     
-    """def queryset(self, vRequest):
-        qs = super(AdminUsuario, self).queryset(vRequest)
-        iEmpresa= Usuario().obtemEmpresaDoUsuario(vRequest.user.id)
-        if iEmpresa != None:
-            return qs.filter(empresa= iEmpresa)
-        else:
-            return qs.all()"""
-    
-    
+    def get_form(self, request, iBla, obj=None, **kwargs):
+        form = super(AdminUsuario,self).get_form(request, obj,**kwargs)
+        iEmpresa= Usuario().obtemEmpresaDoUsuario(request.user.id)
+        form.base_fields['tipo_usuario'].queryset = Tipo_de_Usuario.objects.filter(empresa=iEmpresa)
+        return form 
+
+    def save_model(self, request, obj, form, change):
+        iEmpresa= Usuario().obtemEmpresaDoUsuario(request.user.id)
+        obj.empresa = iEmpresa
+        obj.save()
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
 admin.site.register(Empresa, AdminEmpresa)
 admin.site.register(Tipo_de_Usuario, AdminTipo_de_Usuario)
 admin.site.register(Usuario, AdminUsuario)
--- src/PyProject_GED/urls.py
+++ src/PyProject_GED/urls.py
@@ -1,14 +1,11 @@
 from django.conf.urls.defaults      import patterns, include, url
 from django.conf                    import settings
 from django.contrib                 import admin
-from autenticacao.admin_empresas    import othersite, othersite2
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin_empresa01/', include(othersite.urls)),
-    url(r'^admin_empresa02/', include(othersite2.urls)),
     (r'^media/(.*)$', 'django.views.static.serve',
      {'document_root': settings.MEDIA_ROOT}),
     (r'^', include('flatpages.urls')),
