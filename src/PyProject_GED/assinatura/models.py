31c63abb14f3c60ccc733cad200fb3d1b3d5b127
Diego de Souza Costa <diego.costa@shiftit.com.br>
2012-09-18 19:45:31 -0300

    Adicionado app assinatura e deletada app signature

---
+++ src/PyProject_GED/assinatura/models.py
@@ -0,0 +1,9 @@
+'''
+Created on Jul 18, 2012
+
+@author: Shift IT | www.shiftit.com.br
+'''
+
+
+
+        
---
+++ src/PyProject_GED/assinatura/tests.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Sep 14, 2012
+
+@author: spengler
+'''
+
+from django.test                        import TestCase
+
+import sys
+import chilkat
+
+class Test(TestCase):
+    
+    def setUp(self):
+
+        pass
+    
+    
+    def tearDown(self):
+
+        pass
+    
+    
+    def testAssinaturaPFX(self):
+        print '>>>>>>>>>>>>>>>>>>>>>>> testAssinaturaPFX'
+        cert = chilkat.CkCert()
+        #  Load the cert from a PEM file;
+        cert.LoadPfxFile("/home/diego/Público/certificados/sample.pfx", 'sample')
+
+        #  Use Chilkat Crypt (a non-freeware component) to create
+        #  a digital signature using the certificate w/ private key:
+        crypt = chilkat.CkCrypt2()
+        
+        #  Any string argument automatically begins the 30-day trial.
+        success = crypt.UnlockComponent("30-day trial")
+        if (success != True):
+            print crypt.lastErrorText()
+            sys.exit()
+        
+        #  Tell the crypt component to use this cert.
+        crypt.SetSigningCert(cert)
+        
+        #  A PKCS7 signature for any type of file content can be created:
+        success = crypt.CreateP7S("/home/diego/Público/teste_assinar.odt","/home/diego/Público/teste_assinar.p7s")
+        if (success == False):
+            print crypt.lastErrorText()
+            sys.exit()
+        
+        print crypt.lastErrorText()
+        
+        #  Verify and restore the original file:
+        crypt.SetVerifyCert(cert)
+        
+        success = crypt.VerifyP7S("/home/diego/Público/teste_assinar.odt","/home/diego/Público/teste_assinar.p7s")
+        if (success == False):
+            print crypt.lastErrorText()
+            sys.exit()
+        
+        self.assertEquals(success, True)
+        
+    def testAssinaturaCRT(self):
+        print '>>>>>>>>>>>>>>>>>>>>>>> testAssinaturaCRT'
+        
+        cert = chilkat.CkCert()
+
+        #  Load the cert from a PEM file;
+        cert.LoadPfxFile("/home/diego/Público/certificados/sample.pfx", 'sample')
+        print '>>>>>>>>>>>>>>>>>>>>>>> 01'
+              
+        #pkey = chilkat.CkPrivateKey()
+        #print '>>>>>>>>>>>>>>>>>>>>>>> 02'
+        
+        #  Load the private key from an RSA PEM file:
+        #success = pkey.SavePkcs8File("/home/diego/Público/certificados/certificado.pfx")
+        #print '>>>>>>>>>>>>>>>>>>>>>>> 03'
+        #print success
+        #if (success != True):
+        #    print cert.lastErrorText()
+        #    sys.exit()
+        
+        #  Use Chilkat Crypt (a non-freeware component) to create
+        #  a digital signature using the certificate w/ private key:
+        crypt = chilkat.CkCrypt2()
+        print '>>>>>>>>>>>>>>>>>>>>>>> 04'
+        
+        #  Any string argument automatically begins the 30-day trial.
+        success = crypt.UnlockComponent("30-day trial")
+        print '>>>>>>>>>>>>>>>>>>>>>>> 05'
+        print success
+        if (success != True):
+            print crypt.lastErrorText()
+            sys.exit()
+        
+        #  Tell the crypt component to use this cert.
+        crypt.SetSigningCert(cert)
+        print '>>>>>>>>>>>>>>>>>>>>>>> 06'
+        
+        #  A PKCS7 signature for any type of file content can be created:
+        success = crypt.CreateP7S("/home/diego/Público/teste_assinar.odt","/home/diego/Público/teste_assinar.p7s")
+        print '>>>>>>>>>>>>>>>>>>>>>>> 07'
+        print success
+        if (success == False):
+            print crypt.lastErrorText()
+            sys.exit()
+        
+        print crypt.lastErrorText()
+        
+        #  Verify and restore the original file:
+        crypt.SetVerifyCert(cert)
+        print '>>>>>>>>>>>>>>>>>>>>>>> 08'
+        
+        success = crypt.VerifyP7S("/home/diego/Público/teste_assinar.odt","/home/diego/Público/teste_assinar.p7s")
+        if (success == False):
+            print crypt.lastErrorText()
+            sys.exit()
+        
+        self.assertEquals(success, True)
+        
+    def testVerificarAssinatura(self):
+        crypt = chilkat.CkCrypt2()
+        success = crypt.VerifyP7S("/home/diego/Público/teste_assinar.odt","/home/diego/Público/teste_assinar.p7s")
+        self.assertEquals(success, True)
