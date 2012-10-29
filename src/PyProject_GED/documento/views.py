ff652b6f4555fb4e6b66769541ed51fefc256b32
Diego de Souza Costa <diego.costa@shiftit.com.br>
2012-10-29 12:21:38 -0200

    Digitalizar com 3 btn

--- src/PyProject_GED/documento/views.py
+++ src/PyProject_GED/documento/views.py
@@ -5,6 +5,7 @@ from django.http                        import HttpResponse,\
     HttpResponseRedirect
 from django.contrib.auth.decorators     import login_required
 from django.contrib                     import messages
+from django.conf                        import settings
 
 from PyProject_GED                      import oControle, constantes
 from PyProject_GED.autenticacao.models  import Usuario
@@ -23,19 +24,14 @@ from PyProject_GED.qualidade.models     import Norma, Norma_Documento
 from PyProject_GED.assinatura.models    import Assinatura
 from django.views.decorators.csrf       import csrf_exempt
 from PyProject_GED.envioemail.models    import Email
-
+from PyProject_GED.dynamictwain.models  import TempFormData
 
 import datetime
 import os
 import urllib
-
-from PyProject_GED.envioemail.models import Email
 import json
 import re
-
 import shutil
-from PyProject_GED.dynamictwain.models import TempFormData
-
 
 @login_required 
 def documentos(vRequest, vTitulo):
@@ -571,17 +567,16 @@ def digitalizar(vRequest, vUID=None):
     
     if vRequest.method == 'POST':
         form = FormUploadDeArquivo(vRequest.POST, iIDEmpresa=vRequest.session['IDEmpresa'])
-        if vUID == '0':
+        if vUID != '0':
             vRequest.session['uID']= vUID
-            messages.warning(vRequest, 'Nenhum arquivo Digitalizado para ser adicionado!')
         else:
             if form.is_valid():
                 try :
                     iUID = vRequest.session['uID']
                     iImagemScanner = TempFormData.objects.get(pk=iUID)
                     iArquivo= iImagemScanner.scan
-                    iCaminhoArquivo = iArquivo
-                    iNomeArquivo    = "teste"
+                    iCaminhoArquivo = settings.MEDIA_ROOT + "/" + iArquivo
+                    iNomeArquivo    = os.path.basename(iCaminhoArquivo)
                     #Adicionar na tabela documeto e versao
                     if len(vRequest.POST.get('data_validade')) != 10:
                         iDataValidade= datetime.datetime.now()
