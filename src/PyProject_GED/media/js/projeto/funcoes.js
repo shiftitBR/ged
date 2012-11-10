26216923fcd5ef84b69bbc330718c5c8cfde18ab
Diego de Souza Costa <diego.costa@shiftit.com.br>
2012-11-10 10:09:46 -0200

    Aumentado numero caracteres multiuploader

--- src/PyProject_GED/media/js/projeto/funcoes.js
+++ src/PyProject_GED/media/js/projeto/funcoes.js
@@ -14,37 +14,37 @@ $(document).ready
 		
 		if (window.location.pathname == '/importar/')
 		{
-			$('#id_assunto_importar').keydown(maximoLength('#id_assunto_importar', 100, false));
+			$('#id_assunto_importar').keydown(maximoLength('#id_assunto_importar', 100, true));
 		}
 		
 		if (window.location.pathname == '/encaminhar/')
 		{
-			$('#id_descricao_encaminhar').keypress(maximoLength('#id_descricao_encaminhar', 200, false));
+			$('#id_descricao_encaminhar').keypress(maximoLength('#id_descricao_encaminhar', 200, true));
 		}
 		
 		if (window.location.pathname == '/aprovar_documento/')
 		{
-			$('#aprovar_feedback').keypress(maximoLength('#aprovar_feedback', 200, false));
+			$('#aprovar_feedback').keypress(maximoLength('#aprovar_feedback', 200, true));
 		}
 		
 		if (window.location.pathname == '/reprovar_documento/')
 		{
-			$('#reprovar_feedback').keypress(maximoLength('#reprovar_feedback', 200, false));
+			$('#reprovar_feedback').keypress(maximoLength('#reprovar_feedback', 200, true));
 		}
 		
 		if (window.location.pathname == '/checkin/')
 		{
-			$('#id_descricao_checkin').keypress(maximoLength('#id_descricao_checkin', 200, false));
+			$('#id_descricao_checkin').keypress(maximoLength('#id_descricao_checkin', 200, true));
 		}
 		
 		if (window.location.pathname == '/digitalizar/0/')
 		{
-			$('#id_assunto_digitalizar').keydown(maximoLength('#id_assunto_digitalizar', 100, false));
+			$('#id_assunto_digitalizar').keydown(maximoLength('#id_assunto_digitalizar', 100, true));
 		}
 		
 		if (window.location.pathname == '/importar_lote/')
 		{
-			$('#id_assunto_lote').keydown(maximoLength('#id_assunto_lote', 100, false));
+			$('#id_assunto_lote').keydown(maximoLength('#id_assunto_lote', 100, true));
 		}
 		
 		if ((window.location.pathname).indexOf("visualizar") != -1)
