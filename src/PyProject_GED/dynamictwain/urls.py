6fadfedab0dacb5b8dce694663bd8e97c693155b
Alexandre Spengler <alexandre.spengler@gmail.com>
2012-12-07 17:49:44 -0200

    Excluido app dynamictwain

--- src/PyProject_GED/dynamictwain/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-
-urlpatterns = patterns('',
-    url(r'form/$', 'dynamictwain.views.form', name='form'),
-    url(r'redirect/$', 'dynamictwain.views.redirect', name='redirect'),
-    url(r'upload/$', 'dynamictwain.views.upload', name='upload'),
-    url(r'foobar/$', 'dynamictwain.views.upload', name='foo'),
-)
